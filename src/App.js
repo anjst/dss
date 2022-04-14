20e499150156d2a962da5dce15585741a22e92c1
antanas <antanas24@gmail.com>
2022-04-14 18:21:10 +0300

    soon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCoffee);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p id="coming">coming soon</p>
+        <p id="mail">info@dss.com</p>
+        <a href="https://www.instagram.com/dss_oman/">
+          <FontAwesomeIcon icon="fa-brands fa-instagram" />
         </a>
       </header>
     </div>
